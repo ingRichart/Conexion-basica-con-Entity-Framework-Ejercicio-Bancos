0a4aa0fcd056528323a13fffe62590c4c3a9675f
Rick Hunter <relizalde@bluepeople.com>
2024-06-14 05:58:57 -0600

    //Revisado por RAEH

--- EjercicioBancosAngelIbarra44361/Program.cs
+++ EjercicioBancosAngelIbarra44361/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(opc => opc.UseSqlServer("name=AngelIbarraSQL"));
 
+//Revisado por RAEH
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
